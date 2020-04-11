1f99ae182e36617ebbcb8b8eace913fbb9c600e3
SuperValou <valentin.esmieu@tuta.io>
2020-04-11 19:27:48 +0200

    Add correct aim

---
+++ InTheDark/Assets/Scripts/Weapons/Deadeye.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts.Utilities;
+using UnityEngine;
+
+namespace Assets.Scripts.Weapons
+{
+    public class Deadeye : MonoBehaviour
+    {
+        public Camera eye;
+        public Transform arm;
+
+        public float maxDistance = 100;
+        
+        void Update()
+        {
+            Ray ray = new Ray(eye.transform.position, eye.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, maxDistance))
+            {
+                arm.transform.LookAt(raycastHit.point);
+            }
+            else
+            {
+                arm.transform.LookAt(arm.transform.position + eye.transform.forward);
+            }
+        }
+    }
+}
