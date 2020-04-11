acf1c3cc83f69bc2ff31eb86feaf13a9a94c1d6e
SuperValou <valentin.esmieu@tuta.io>
2020-04-11 20:42:30 +0200

    Add a robot-looking foe

--- InTheDark/Assets/Scripts/Foes/FoeManager.cs
+++ InTheDark/Assets/Scripts/Foes/FoeManager.cs
@@ -8,13 +8,16 @@ namespace Assets.Scripts.Foes
 
         void Start()
         {
+            SpawnFoe();
+            SpawnFoe();
+            SpawnFoe();
             InvokeRepeating(nameof(SpawnFoe), 0, 3);
         }
 
         private void SpawnFoe()
         {
             var rand = Random.insideUnitCircle * 20;
-            var pos = new Vector3(Mathf.Max(2, rand.x), 2, Mathf.Max(2, rand.y));
+            var pos = new Vector3(Mathf.Max(3, rand.x), 2, Mathf.Max(3, rand.y));
             Instantiate(foePrefab, pos, Quaternion.identity);
         }
     }
