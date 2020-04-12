8ef5b891a86095140c4123273a432194b5d03f79
SuperValou <valentin.esmieu@tuta.io>
2020-04-12 11:18:21 +0200

    Trying to make foes blink when damaged, but it doesn't work

--- InTheDark/Assets/Scripts/Foes/Foe.cs
+++ InTheDark/Assets/Scripts/Foes/Foe.cs
@@ -1,11 +1,18 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace Assets.Scripts.Foes
 {
     public class Foe : MonoBehaviour
     {
+        public Material hitMaterial;
         public float maxHealth = 20;
         
+        private MeshRenderer[] _renderers;
+        private Material[] _materials;
+
         public float CurrentHealth { get; private set; }
 
         public bool IsAlive => CurrentHealth > 0;
@@ -13,11 +20,34 @@ namespace Assets.Scripts.Foes
         void Start()
         {
             CurrentHealth = maxHealth;
+            _renderers = this.GetComponentsInChildren<MeshRenderer>();
+
+            var materials = _renderers.SelectMany(r => r.materials).ToList();
+            _materials = new Material[materials.Count];
+
+            int index = 0;
+            foreach (var material in materials)
+            {
+                _materials[index] = material;
+                index++;
+            }
         }
 
         public void TakeDamage(float damages)
         {
             CurrentHealth -= damages;
+
+            Debug.Log("ARGH");
+            foreach (var meshRenderer in _renderers)
+            {
+                for (int i = 0; i < meshRenderer.materials.Length; i++)
+                {
+                    meshRenderer.materials[i] = hitMaterial;
+                    Debug.Log($"{meshRenderer.gameObject.name} - material {i} : {meshRenderer.materials[i].name}");
+                }
+            }
+
+            //Invoke(nameof(RestoreColors), 3);
 
             if (CurrentHealth < 0)
             {
@@ -29,6 +59,19 @@ namespace Assets.Scripts.Foes
         private void Die()
         {
             Destroy(gameObject);
+        }
+
+        private void RestoreColors()
+        {
+            int index = 0;
+            foreach (var meshRenderer in _renderers)
+            {
+                for (int i = 0; i < meshRenderer.materials.Length; i++)
+                {
+                    meshRenderer.materials[i] = _materials[index];
+                    index++;
+                }
+            }
         }
     }
 }
--- InTheDark/Assets/Scripts/Foes/FoeManager.cs
+++ InTheDark/Assets/Scripts/Foes/FoeManager.cs
@@ -17,7 +17,7 @@ namespace Assets.Scripts.Foes
         private void SpawnFoe()
         {
             var rand = Random.insideUnitCircle * 20;
-            var pos = new Vector3(Mathf.Max(3, rand.x), 2, Mathf.Max(3, rand.y));
+            var pos = new Vector3(Mathf.Max(5, rand.x), 2, Mathf.Max(5, rand.y));
             Instantiate(foePrefab, pos, Quaternion.identity);
         }
     }
