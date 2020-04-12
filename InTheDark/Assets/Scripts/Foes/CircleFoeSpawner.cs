5cf41f5c625aa603a8ae12c31a06fa767d55bf9c
SuperValou <valentin.esmieu@tuta.io>
2020-04-12 18:29:37 +0200

    Create Foe spawner

---
+++ InTheDark/Assets/Scripts/Foes/CircleFoeSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.Foes
+{
+    public class CircleFoeSpawner : MonoBehaviour
+    {
+        public Foe foePrefab;
+        public float spawnPerSecond = 0.3f;
+        public float circleRadius = 20;
+        public int initialBurstSpawn = 3;
+
+        void Start()
+        {
+            for (int i = 0; i < initialBurstSpawn; i++)
+            {
+                SpawnFoe();
+            }
+
+            if (spawnPerSecond > 0)
+            {
+                InvokeRepeating(nameof(SpawnFoe), 0, spawnPerSecond);
+            }
+        }
+
+        private void SpawnFoe()
+        {
+            var rand = Random.insideUnitCircle.normalized * circleRadius;
+            var pos = new Vector3(rand.x, 0.2f, rand.y);
+            Instantiate(foePrefab, pos, Quaternion.identity);
+        }
+    }
+}
---
+++ InTheDark/Assets/Scripts/Foes/PointsFoeSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Foes
+{
+    public class PointsFoeSpawner : MonoBehaviour
+    {
+        public Foe foePrefab;
+
+        public Transform[] spawnPoints;
+        public AnimationCurve spawnPerSecondOverTime = new AnimationCurve(new Keyframe(0f, 0.1f, 0f, 0f), new Keyframe(60f, 1f, 0f, 0f));
+
+        void Start()
+        {
+            if (spawnPoints.Length > 0)
+            {
+                StartCoroutine(SpawnRoutine());
+            }
+            else
+            {
+                Debug.LogWarning("No spawn point.");
+            }
+            
+        }
+
+        private IEnumerator SpawnRoutine()
+        {
+            while (this.gameObject != null)
+            {
+                float spawnFrequency = spawnPerSecondOverTime.Evaluate(Time.time);
+                if (spawnFrequency <= 0)
+                {
+                    yield return null;
+                }
+
+                float timeToWait = 1f / spawnFrequency;
+                yield return new WaitForSeconds(timeToWait);
+                SpawnFoe();
+            }
+        }
+
+        private void SpawnFoe()
+        {
+            Vector3 position = PickSpawnPoint();
+            Instantiate(foePrefab, position, Quaternion.identity);
+        }
+
+        private Vector3 PickSpawnPoint()
+        {
+            int randomIndex = (int) (Random.value * spawnPoints.Length);
+            return spawnPoints[randomIndex].position;
+        }
+    }
+}
